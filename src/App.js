c49c10c72126cdd7e654dc17639980d1b15d5aff
Saúl Moreno <saul.morenin@outlook.es>
2023-01-17 15:23:26 +0100

    Electron Setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
 
 //import PrivateRoute from './Helpers/PrivateRoute';
 
--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,11 +1,8 @@
 
-
-const Dashboard = () => {
-    return(
-
-        <div>Hello World!</div>
-
+export default function Dashboard() {
+    return (
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     )
-}
-
-export default Dashboard;
+  }
