983607f9ac093a7f149883b8437c2a8aab229a8d
Saúl Moreno <saul.morenin@outlook.es>
2023-01-17 23:19:59 +0100

    Auth Added

--- server/api/config/config.js
+++ server/api/config/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 4000,
-    DB: process.env.DB || 'mongodb://127.0.0.1:27017/',
+    DB: process.env.DB || 'mongodb://127.0.0.1:27017/dansa',
+    TOKEN_SECRET: process.env.PORT || "TOKEN_SECRETT"
 }
--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
-//import PrivateRoute from './Helpers/PrivateRoute';
+import PrivateRoute from './Helpers/PrivateRoute';
 
 //Pages
 import Login from './Pages/Login';
 import Dashboard from './Pages/Dashboard';
 
+//Recordar Probar AUTH "/" -> Private route y funciona
+
 export default function App() {
   return (
     <div className="App">
-        <Router>
-          <Routes>
-            <Route path='/' element={<Dashboard/>}/>
-            <Route path='/login' element={<Login/>}/>
-          </Routes>
-        </Router>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/dashboard" element={<PrivateRoute><Dashboard/></PrivateRoute>}/>
+        </Routes>
+      </Router>
+    </div>
   );
 }
